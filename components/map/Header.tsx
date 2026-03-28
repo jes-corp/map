6c0e3bfcdcd745df2eed6e3bfead29d188b16f1b
stevan23k <esteproo@gmail.com>
2026-03-27 19:37:54 -0500

    online

--- components/map/Header.tsx
+++ components/map/Header.tsx
@@ -2,9 +2,11 @@
 import { Button } from "@/components/ui/button";
 import { User } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useSocketStore } from "@/store/socketStore";
 
 export default function MapHeader() {
     const router = useRouter();
+    const { isConnected } = useSocketStore();
 
     return (
         <header className="w-full fixed top-0 z-10 flex justify-center px-6">
@@ -14,17 +16,22 @@ export default function MapHeader() {
                     MAP
                 </div>
 
-                <div>
-                    <Button
-                        variant="ghost"
-                        size="sm"
-                        className="rounded-full cursor-pointer h-10 w-10"
-                        onClick={() => router.push("/profile")}
-                    >
-                        <User size={32} />
+                <div className="flex flex-row items-center gap-2">
+                    <div className="flex flex-row items-center gap-2">
+                        <div className={`h-3 w-3 rounded-full ${isConnected ? "bg-green-500" : "bg-red-500"}`}></div>
+                        <p className={`text-sm font-semibold ${isConnected ? "text-green-500" : "text-red-500"}`}>{isConnected ? "Conectado" : "Desconectado"}</p>
+                    </div>
+                    <div>
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            className="rounded-full cursor-pointer h-10 w-10"
+                            onClick={() => router.push("/profile")}
+                        >
+                            <User size={32} />
 
-                    </Button>
-
+                        </Button>
+                    </div>
                 </div>
             </div>
         </header >
